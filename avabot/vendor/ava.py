2e31411f8ada456e9e0701d9cfeff509ff6a62e3
David Vuong <david.vuong256@gmail.com>
2017-05-16 00:08:23 +1000

    refactor(vendor/ava): tidy up ava api
    - add type annotations
    - stop hiding important response objects

--- avabot/vendor/ava.py
+++ avabot/vendor/ava.py
@@ -1,32 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+from typing import Optional, Callable, Dict
+
 import os
 import json
-import requests
 import datetime
 import time
 import logging
 
+import requests
 
-class AvaAPIAuth(object):
+from ..domain.ava_exceptions import AvaInvalidTokenException
+
+
+class AvaApiAuth:
     AUTH_TOKEN_REFRESH_THRESHOLD = 1000 * 30  # 0.5 minutes in milliseconds
 
-    def __init__(self, client_id, client_secret, endpoint, version):
+    def __init__(self, client_id: str, client_secret: str, endpoint: str, version: str) -> None:
         self.client_id = client_id
         self.client_secret = client_secret
         self.endpoint = endpoint
         self.version = version
         self._auth = {}
 
-    def is_expired(self):
+    def is_expired(self) -> bool:
         if not self._auth:
             return True
 
         now = int(datetime.datetime.now().strftime('%s')) * 1000
         return now >= (self._auth['expires_at'] - self.__class__.AUTH_TOKEN_REFRESH_THRESHOLD)
 
-    def request_new_token(self):
+    def request_new_token(self) -> bool:
         auth_endpoint = os.path.join(self.endpoint, self.version, 'oauth/token')
         payload = {'clientId': self.client_id, 'clientSecret': self.client_secret}
 
@@ -43,61 +48,70 @@ class AvaAPIAuth(object):
         return True
 
     @property
-    def token(self):
+    def token(self) -> str:
         if self.is_expired():
-            logging.debug('token is expired, refreshing token')
             self.request_new_token()
         return self._auth['token']
 
 
-class AvaAPI(object):
+class AvaApi:
     ERROR_STATUSES = ['PARTIAL_ERROR', 'ERROR']
     IN_PROGRESS_STATUS = 'IN_PROGRESS'
 
-    def __init__(self, client_id, client_secret, endpoint, version):
-        self.auth = AvaAPIAuth(client_id, client_secret, endpoint, version)
+    def __init__(self, client_id: str, client_secret: str, endpoint: str, version: str):
+        self.auth = AvaApiAuth(client_id, client_secret, endpoint, version)
 
         self.endpoint = endpoint
         self.version = version
 
-    def _api_request(self, method, endpoint, payload=None, headers=None):
+    def _api_request(
+        self,
+        method: Callable,
+        endpoint: str,
+        payload: Optional[Dict] = None,
+        headers: Optional[Dict] = None
+    ) -> Dict:
         payload = payload or {}
         headers = headers or {}
 
         token = self.auth.token
         if not token:
-            return None
+            raise AvaInvalidTokenException('failed to retrieve ava api token')
 
         headers['Authorization'] = 'Bearer %s' % token
         response = method(endpoint, data=json.dumps(payload), headers=headers)
-        logging.debug('received response code: %s (%s)' % (response.status_code, endpoint))
+        logging.info('received response code: %s (%s)' % (response.status_code, endpoint))
 
-        return response.json() if response.status_code == 200 else None
-
-    def detect(self, url, webhook_url):
-        payload = {
-            'items': [{'url': url}], 'webhookUrl': webhook_url, 'model': 'CLASSIFIER_GEN_01',
+        return {
+            'body': response.json(),
+            'code': response.status_code,
         }
+
+    def detect(self, url: str, model: Optional[str] = None) -> Dict:
+        payload = {'items': [{'url': url}], 'model': model}
         endpoint = os.path.join(self.endpoint, self.version, 'detect')
-        headers = {
-            'Content-Type': 'application/json',
-        }
+        headers = {'Content-Type': 'application/json'}
 
-        response = self._api_request(requests.post, endpoint, payload, headers)
-        return response['id'] if response is not None else None
+        return self._api_request(requests.post, endpoint, payload, headers)
 
-    def get_image_result(self, job_id):
+    def get_image_result(self, job_id: str) -> Dict:
         endpoint = os.path.join(self.endpoint, self.version, 'detect', job_id)
-        response = self._api_request(requests.get, endpoint)
-        return response if response is not None else {}
+        return self._api_request(requests.get, endpoint)
 
-    def poll_until_complete(self, job_id):
-        tag_result = self.get_image_result(job_id)
-        while tag_result and tag_result['status']['code'] == self.__class__.IN_PROGRESS_STATUS:
-            tag_result = self.get_image_result(job_id)
-            logging.info('status for "%s": %s' % (job_id, tag_result.get('status', 'N/A')))
+    def _should_continue_poll(self, response: Dict) -> bool:
+        body = response['body']
+        status_code = response['code']
+        if status_code != 200:
+            return False
+
+        job_status_code = body['results'][0]['status']['code']
+        return job_status_code == self.__class__.IN_PROGRESS_STATUS
+
+    def poll_until_complete(self, job_id: str) -> Dict:
+        response = self.get_image_result(job_id)
+
+        while self._should_continue_poll(response):
+            logging.info('polling for job (%s) to complete' % job_id)
+            response = self.get_image_result(job_id)
             time.sleep(0.5)
-
-        if not tag_result or tag_result['status']['code'] in self.__class__.ERROR_STATUSES:
-            return None
-        return tag_result
+        return response
