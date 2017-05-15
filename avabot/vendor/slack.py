bac26d48287fafe145013123a553fec2b3ebfcee
David Vuong <david.vuong256@gmail.com>
2017-05-15 17:22:28 +1000

    feat(vendor/slack): add slack adaptor for message processing
    This might look a little confusing but I think it works pretty well:
    
    Instead of having the slackbot library, we write a custom one of our
    own. This gives us more freedom regarding how we structure our project
    at the downside of having to write a bit of parsing on our own.
    
    How does it work?
    
    A Slack instance is instantiated with the SlackConfig and a
    MessageParser object.
    
    slack.listen(handler) is then called to begin listening to messages from Slack
    via `@slackbot name`. When a message is received, a bit if
    pre-processing is handled by the generic Slack instance. Afterwards, the
    message is passed to the specified MessageHandler.
    
    The MessageHandler can be extended to support multiple types of
    messaging parsing. Right now, I've adapted it over docopt (effectively
    treating the slackbot interface like a POSIX compliant CLI).
    
    If successful, a dict with the parsed message is passed to the handler
    specified in the `.listen` call. This handler through a series of if
    else statements can figure out what piece of command to execute.
    
    Bugs
    
    There is one bug responses from the bot to not correctly parse @user
    section in the Slack message. Not super critical so I've decided to
    commit these changes without fixing it.
    
    Next thing to work on
    
    Implement the handler function to deal with the processed message

--- avabot/vendor/slack.py
+++ avabot/vendor/slack.py
@@ -1,9 +1,78 @@
 # -*- coding: utf-8 -*-
-from slackbot.bot import settings
+import logging
+import time
 
-config = settings.config
-DEFAULT_WHITELIST_CHANNELS = config.get('WHITELIST_CHANNELS')
+from typing import List, Dict, Optional, Callable
+
+from slackclient import SlackClient
+from ..config import SlackConfig
+from ..services.parsers import MessageParser
+
+from ..domain.parse_exceptions import AvaSlackbotException
+
+ParsedSlackMessage = Optional[Dict]
+MessageHandler = Callable[[Dict], None]
 
 
-def should_respond_to_message(message, whitelist=DEFAULT_WHITELIST_CHANNELS):
-    return message.body['channel'] in whitelist
+class Slack:
+    def __init__(self, config: SlackConfig, message_parser: MessageParser) -> None:
+        self.config = config
+        self.client = SlackClient(config.api_token)
+        self.message_parser = message_parser
+
+    def _should_respond(self, message: Dict):
+        user = message.get('user')
+        type_ = message.get('type')
+        text = message.get('text')
+
+        if not message:
+            return False
+        if not text:
+            return False
+        if not type_ or type_ != 'message':
+            return False
+        if not user or user == self.config.bot_id:
+            return False
+        return True
+
+    def _format_invalid_message(self, message: Dict, error_message: str) -> str:
+        return '\n'.join([
+            '<@%s> Bad command, RTFM! `@ava --help` for more details. See usage below:' % message['user'],
+            '```',
+            error_message,
+            '```',
+        ])
+
+    def _parse_message(self, message: Dict) -> Optional[ParsedSlackMessage]:
+        if not self._should_respond(message):
+            return None
+
+        text = message['text']
+        channel = message['channel']
+        user = message['user']
+        logging.info('received acceptable message from user "%s"' % text)
+
+        try:
+            return self.message_parser.run(text, channel, user)
+        except AvaSlackbotException as e:
+            logging.info('"%s" deemed an invalid message by our message_parser' % text)
+            response = self._format_invalid_message(message, str(e))
+            self.client.api_call('chat.postMessage', channel=channel, text=response, as_user=True, parse='full')
+            return None
+
+    def _process_messages(self, messages: List[Dict], handler: MessageHandler) -> None:
+        processed_messages = map(self._parse_message, messages)
+        processed_messages = filter(None.__ne__, processed_messages)
+        processed_messages = map(handler, processed_messages)
+        list(processed_messages)
+
+    def listen(self, handler: MessageHandler) -> None:
+        if self.client.rtm_connect():
+            logging.info('connected to slack, ready to accept messages')
+            while True:
+                slack_messages = self.client.rtm_read()
+                self._process_messages(slack_messages, handler)
+                time.sleep(self.config.websocket_delay)
+        else:
+            logging.error('failed to connect to slack, perhaps invalid token')
+            raise RuntimeError()
