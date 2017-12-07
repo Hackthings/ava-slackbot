f8054aa8f91c5795ad7e3d28750c75579ad5db13
David Vuong <david.vuong256@gmail.com>
2017-12-07 11:19:06 +1100

    rm(parsers): remove search, find-person, and consensus

--- avabot/services/parsers.py
+++ avabot/services/parsers.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import shlex
+
 import docopt
 
-from avabot.domain.exceptions.parse import ParseCommandException
 from avabot import __author__, __author_email__
 from avabot import __version__
+from avabot.exceptions.parse import ParseCommandException
 
 
 class Parser:
@@ -16,32 +17,22 @@ class MessageParser(Parser):
     __doc__ = """Ava Slackbot
 
 Usage:
-    @ava (c|consensus) <url> [--raw-json] [--all] [--head=<n>]
-    @ava (d|detect) <url> [--model=<model> --mversion=<version>] [--raw-json] [--head=<n>]
-    @ava (fp|find-person) <url> [--model=<model>] [--raw-json]
-    @ava (s|search) <id>
+    @ava (d|detect) <url> [--raw-json]
     @ava (-h|--help|-v|--version)
 
 Commands:
-    (c|consensus)         request against /v1/detect over all available models (except beta)
-    (d|detect)            request against /v1/detect
-    (fp|find-person)      request against /v1/find-person
-    (s|search)            performs a search for the given <custom_id>
+    (d|detect)    request against /v1/detect
 
 Arguments:
-    <url>                 image url you want to run detections on
-    <id>                  a job id or custom id to search against
+    <url>         image url you want to run detections on
+    <id>          a job id or custom id to search against
 
 Options:
-    -h --help             shows this
-    -v --version          shows version
+    -h --help     shows this
+    -v --version  shows version
 
-    -a --all              shows all objects the NN model returned
-    --head=<n>            truncates to the top number of objects returned
-    --model=<model>       the NN model to run detections with
-    --mversion<=version>  the version uuid for the given --model
-
-    -raw-json             returns the raw JSON response from the Image Intelligence API
+    -a --all      shows all objects the NN model returned
+    -raw-json     returns the raw JSON response from the Image Intelligence API
 
 Author: %s <%s>, Image Intelligence
 GitHub: https://github.com/ImageIntelligence/ava-slackbot
